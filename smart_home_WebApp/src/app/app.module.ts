453814b7346d7e2a9ff90703e1c71467b51c760e
Ana Carolina Manso Silvério <acmsilverio@sga.pucminas.br>
2022-04-22 16:51:17 -0300

    adding initial components and routes

--- smart_home_WebApp/src/app/app.module.ts
+++ smart_home_WebApp/src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Error404Compnent } from './core/error-404/error-404.component';
+import { AirConditionerComponent } from './smart-home-components/air-conditioner/air-conditioner.component';
+import { LightsComponent } from './smart-home-components/lights/lights.component';
+import { SmartDoorLockComponent } from './smart-home-components/smart-door-lock/smart-door-lock.component';
+import { SmartHomeMenuComponent } from './smart-home-menu/smart-home-menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SmartHomeMenuComponent,
+    LightsComponent,
+    AirConditionerComponent,
+    SmartDoorLockComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo : 'smartHome/menu', pathMatch : 'full'
+      },
+      {
+        path : 'smartHome/menu', component : SmartHomeMenuComponent 
+      },
+       
+       {
+         path : 'smartHome/lights', component: LightsComponent
+       }, 
+       {
+        path : 'smartHome/ac', component: AirConditionerComponent
+      }, 
+      {
+        path : 'smartHome/smartLock', component: SmartDoorLockComponent
+      },
+      {
+        path: '**', component : Error404Compnent
+      }, 
+    
+   
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ smart_home_WebApp/src/app/smart-home-menu/smart-home-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-home-menu',
+  templateUrl: './smart-home-menu.component.html',
+  styleUrls: ['./smart-home-menu.component.css']
+})
+export class SmartHomeMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
