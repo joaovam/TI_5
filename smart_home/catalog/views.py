537bc36b5d81a8852b2ab53a519582a617ed3164
Joao Amorim <amorimvictorjoao3@gmail.com@users.noreply.github.com>
2022-05-23 22:35:06 -0300

    enviando

--- smart_home/catalog/views.py
+++ smart_home/catalog/views.py
@@ -36,28 +36,41 @@ def set_device(original_device, new_device):
         temperature_changed = True
 
     response_device = dict(ID=new_device.ID)
+    status = 0 if not new_device.status else 1
     if status_changed:
         response_device['status'] = new_device.status
         print("status foi alterado")
-        status = 0 if not new_device.status else 1
+
         if new_device.type_device =="luz":
             print(f"http://{arduino.address}/luz?status={status}&id={new_device.ID}")
             requests.get(f"http://{arduino.address}/luz?status={status}&id={new_device.ID}")
 
         elif new_device.type_device == "tranca":
-            requests.get(f"http://{arduino.address}/tranca?status={status}")
             print(f"http://{arduino.address}/tranca?status={status}")
+            requests.get(f"http://{arduino.address}/tranca?status={status}")
+
         else:
             if temperature_changed:
                 response_device['temperature'] = new_device.temperature
-                print(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={new_device.status}")
-                requests.get(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={new_device.status}")
                 print("temperatura foi alterada")
+                print(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+                requests.get(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+
             else:
-                print(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={new_device.status}")
-                requests.get(
-                    f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={new_device.status}")
                 print("temperatura nao foi alterada")
+                print(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+                requests.get(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+    else:
+        if temperature_changed:
+            response_device['temperature'] = new_device.temperature
+            print("temperatura foi alterada")
+            print(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+            requests.get(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+
+        else:
+            print("temperatura nao foi alterada")
+            print(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
+            requests.get(f"http://{arduino.address}/AC?temperatura={new_device.temperature}&status={status}")
 
 
     print("==================== fazer request para arduino============")
@@ -78,14 +91,15 @@ def change_option(request):
         if device.ID is body['ID']:
             print(f"device inicial{device}")
             new_device = Device.from_json(body)
-            set_device(device, new_device)
-            arduino.devices.remove(device)
-            device = new_device
-            arduino.devices.append(new_device)
-            print(f"novo device{new_device}")
-            print(f"device final:{device}")
-
-            return JsonResponse(body)
+            try:
+                set_device(device, new_device)
+            except:
+                print("Exception encontrada, verificar endereço do arduino")
+            finally:
+                arduino.devices.remove(device)
+                device = new_device
+                arduino.devices.append(new_device)
+                return JsonResponse(body)
 
     return HttpResponse("Dispositivo não encontrado")
 
